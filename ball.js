3de1ffb2f4b20d0202171c0b29819f42926e0624
Nicola Eschbach <nicola.eschbach@hotmail.com>
2026-01-08 23:16:57 +0100

    make racket click real and smoother ball movement

--- ball.js
+++ ball.js
@@ -8,24 +8,33 @@ document.addEventListener("click", (e) => {
     let y = e.clientY - 12.5;
     ball.style.left = `${x}px`;
     ball.style.top = `${y}px`;
+    const randomSpeed = () => {
+        const sign = Math.random() < 0.5 ? -1 : 1; // negative oder positive Range
+        const min = 0.5;
+        const max = 0.8;
+        return sign * (min + Math.random() * (max - min));
+    }
 
-    let xSpeed = (Math.random() - 0.5) * 25; // horizontal speed
-    let ySpeed = (Math.random() - 0.5) * 25; // vertical speed
-    const spinSpeed = (Math.random() - 0.3) * 80; // degrees per frame
+    const speedDistribution = Math.random()
+    let xSpeed = speedDistribution * 30 * randomSpeed();
+    let ySpeed = (1 - speedDistribution) * 30 * randomSpeed();
 
-    const accelerationX = 1.011; // speed multiplier per frame
-    const accelerationY = 1.029; // speed multiplier per frame
+    console.log(ySpeed, xSpeed)
+    const randomSign = Math.random() < 0.5 ? -1 : 1;
+    let zSpeed = 1.0;
 
+    const accelerationX = 1.02;
+    const accelerationY = 1.02;
+    const accelerationZ = 0.01 * randomSign;
     function animate() {
         xSpeed *= accelerationX;
         ySpeed *= accelerationY;
-
+        zSpeed += accelerationZ;
         x += xSpeed;
         y += ySpeed;
 
-        const rotate = spinSpeed * 6;
 
-        ball.style.transform = `translate(${x - e.clientX + 12.5}px, ${y - e.clientY + 12.5}px) rotate(${rotate}deg)`;
+        ball.style.transform = `translate(${x - e.clientX + 12.5}px, ${y - e.clientY + 12.5}px) scale(${zSpeed})`;
 
         const rect = ball.getBoundingClientRect();
         if (rect.top > window.innerHeight || rect.bottom < 0 || rect.left > window.innerWidth || rect.right < 0) {
@@ -71,12 +80,29 @@ if (!isTouchDevice) {
 document.addEventListener("mousedown", () => {
     cursorBall.style.transform = `translate(-50%, -50%) scale(1.2)`;
     cursorBall.style.opacity = `80%`;
+    if (cursorBall.classList.contains("cursor-racket")) {
+        cursorBall.style.transform = "rotateX(-60deg) rotateY(10deg)";;
+        console.log("rotete")
+    }
 
 });
 document.addEventListener("mouseup", () => {
     cursorBall.style.transform = `translate(-50%, -50%) scale(1)`;
     cursorBall.style.opacity = `100%`;
 
+
+
 });
 
 
+document.addEventListener("DOMContentLoaded", () => {
+    const heroBg = document.querySelector(".hero-bg"); // dein div hat keine ID
+
+    if (!heroBg) return; // Sicherheit
+
+    document.addEventListener("mousemove", (e) => {
+        const x = (e.clientX / window.innerWidth - 0.5) * 30; // max ±10px
+        const y = (e.clientY / window.innerHeight - 0.5) * 20;
+        heroBg.style.transform = `scale(1.1) translate(${x}px, ${y}px)`;
+    });
+});
