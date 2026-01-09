49e67709e0173357e9155af7b62c1b1ec606f2af
Nicola Eschbach <nicola.eschbach@hotmail.com>
2026-01-09 02:07:26 +0100

    refactor html and css

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ document.addEventListener("click", (e) => {
     const ball = document.createElement("div");
     ball.classList.add("ball");
     document.body.appendChild(ball);
-    let x = e.clientX - 12.5;
-    let y = e.clientY - 12.5;
+    let x = e.clientX;
+    let y = e.clientY;
     ball.style.left = `${x}px`;
     ball.style.top = `${y}px`;
+    // ball.style.transform = 'translateX(-50%) translateY(-50%)'
     const randomSpeed = () => {
-        const sign = Math.random() < 0.5 ? -1 : 1; // negative oder positive Range
+        const sign = Math.random() < 0.5 ? -1 : 1;
         const min = 0.5;
         const max = 0.8;
         return sign * (min + Math.random() * (max - min));
@@ -18,23 +19,20 @@ document.addEventListener("click", (e) => {
     const speedDistribution = Math.random()
     let xSpeed = speedDistribution * 30 * randomSpeed();
     let ySpeed = (1 - speedDistribution) * 30 * randomSpeed();
-
-    console.log(ySpeed, xSpeed)
-    const randomSign = Math.random() < 0.5 ? -1 : 1;
     let zSpeed = 1.0;
+
+    const randomSign = Math.random() - 0.4;
 
     const accelerationX = 1.02;
     const accelerationY = 1.02;
-    const accelerationZ = 0.01 * randomSign;
+    const accelerationZ = 0.05 * randomSign;
     function animate() {
         xSpeed *= accelerationX;
         ySpeed *= accelerationY;
         zSpeed += accelerationZ;
         x += xSpeed;
         y += ySpeed;
-
-
-        ball.style.transform = `translate(${x - e.clientX + 12.5}px, ${y - e.clientY + 12.5}px) scale(${zSpeed})`;
+        ball.style.transform = `translate(${x - e.clientX}px, ${y - e.clientY}px) scale(${zSpeed})`;
 
         const rect = ball.getBoundingClientRect();
         if (rect.top > window.innerHeight || rect.bottom < 0 || rect.left > window.innerWidth || rect.right < 0) {
@@ -43,7 +41,6 @@ document.addEventListener("click", (e) => {
             requestAnimationFrame(animate);
         }
     }
-
     requestAnimationFrame(animate);
 });
 
@@ -59,6 +56,7 @@ if (!isTouchDevice) {
     document.addEventListener("mousemove", (e) => {
         cursorBall.style.left = `${e.clientX}px`;
         cursorBall.style.top = `${e.clientY}px`;
+        cursorBall.style.transform = 'translateX(-50%) translateY(-50%)'
     });
 }
 
@@ -81,27 +79,24 @@ document.addEventListener("mousedown", () => {
     cursorBall.style.transform = `translate(-50%, -50%) scale(1.2)`;
     cursorBall.style.opacity = `80%`;
     if (cursorBall.classList.contains("cursor-racket")) {
-        cursorBall.style.transform = "rotateX(-60deg) rotateY(10deg)";;
-        console.log("rotete")
+        cursorBall.style.transform = "rotateX(-60deg) rotateY(10deg) rotateZ(20deg) scale(0.95)";;
     }
 
 });
 document.addEventListener("mouseup", () => {
-    cursorBall.style.transform = `translate(-50%, -50%) scale(1)`;
     cursorBall.style.opacity = `100%`;
+    cursorBall.style.transform = "rotateX(0deg) rotateY(0deg) translate(-50%, -50%) scale(1)";;
 
 
 
 });
 
-
 document.addEventListener("DOMContentLoaded", () => {
-    const heroBg = document.querySelector(".hero-bg"); // dein div hat keine ID
-
-    if (!heroBg) return; // Sicherheit
+    const heroBg = document.querySelector(".hero-bg");
+    if (!heroBg) return;
 
     document.addEventListener("mousemove", (e) => {
-        const x = (e.clientX / window.innerWidth - 0.5) * 30; // max ±10px
+        const x = (e.clientX / window.innerWidth - 0.5) * 30;
         const y = (e.clientY / window.innerHeight - 0.5) * 20;
         heroBg.style.transform = `scale(1.1) translate(${x}px, ${y}px)`;
     });
